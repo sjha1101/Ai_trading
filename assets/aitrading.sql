377d834888665e133598571afcde61f8b68c5805
sjha1101 <codingchannel8i@gmail.com>
2025-07-16 19:25:42 +0530

    Update in homepage

---
+++ assets/aitrading.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE aitrading;
+USE aitrading;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    phone_number VARCHAR(15) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    pan_tax_id VARCHAR(20) NOT NULL UNIQUE,
+    country_address TEXT NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    confirm_password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE stocks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    symbol_image VARCHAR(255),         -- Path or URL to the company logo
+    company VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    change_value VARCHAR(10),
+    suggestion ENUM('Buy', 'Hold', 'Avoid') DEFAULT 'Hold'
+);
+
+INSERT INTO stocks (symbol_image, company, price, change_value, suggestion)
+VALUES ('/assets/images/tcs.png', 'Tata Consultancy Services', 3700.00, '+15.00', 'Buy');
+
